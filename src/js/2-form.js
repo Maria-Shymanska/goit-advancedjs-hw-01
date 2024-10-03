cb6614a181fb11017dbe66d62f35c31f8d2ae35e
Shymanska_Maria <shymanskam524@gmail.com>
2024-10-03 12:40:16 +0200

    Update 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,38 +1,41 @@
 // Оголошення об'єкта formData з порожніми значеннями
-
 let formData = {
-    email: "",
-    message: ""
+  email: '',
+  message: '',
 };
 
 const form = document.querySelector('.feedback-form');
 
-// перевірка локального сховища при завантаженні сторінки
+// Перевірка локального сховища при завантаженні сторінки
 window.addEventListener('load', () => {
-    const saveData = localStorage.getItem('feedback-form-state');
-    if (saveData) {
-        formData.JSON.parse(saveData);
-        form.elements.email.value = formData.email;
-        form.elements.message.value = formData.message;
+  const savedData = localStorage.getItem('feedback-form-state');
+  if (savedData) {
+    try {
+      formData = JSON.parse(savedData); 
+      form.elements.email.value = formData.email || '';
+      form.elements.message.value = formData.message || '';
+    } catch (error) {
+      console.error('Error parsing saved data:', error);
     }
+  }
 });
 
-// Зберігання formData у локальне сховище
- 
+// Функція збереження formData у локальне сховище
 const saveLocalStorage = () => {
-    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 };
 
 // Делегування події 'input' для відстеження змін у полях форми
-
-form.addEventListener('input', (e) => {
-    formData[e.target.name] = e.target.value
-    saveLocalStorage();
+form.addEventListener('input', e => {
+  formData[e.target.name] = e.target.value; 
+  saveLocalStorage(); 
 });
 
+// Обробка події 'submit'
 form.addEventListener('submit', e => {
   e.preventDefault();
 
+  // Валідація електронної пошти та повідомлення
   if (!validator.isEmail(formData.email)) {
     alert('Please enter a valid email address');
     return;
@@ -43,19 +46,13 @@ form.addEventListener('submit', e => {
     return;
   }
 
+
   console.log(formData);
 
-  try {
-    // Очищення форми, об'єкта formData і локального сховища
-    localStorage.removeItem('feedback-form-state');
-  } catch (error) {
-    console.error('LocalStorage is not available:', error);
-  }
-
-  // Очищення об'єкта formData
+  // Очищення локального сховища та formData після відправлення форми
+  localStorage.removeItem('feedback-form-state');
   formData = { email: '', message: '' };
 
   // Скидання форми
   form.reset();
 });
-
